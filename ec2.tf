f3a9d9962101a2e8a8acf071ec4e181e590b85c5
esiddiqi <siddiquiemaad@gmail.com>
2024-02-24 15:44:31 +0530

    updated port

--- ec2.tf
+++ ec2.tf
@@ -24,6 +24,14 @@ resource "aws_security_group" "demo-sg" {
     to_port     = 22
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 8080
+    protocol    = "tcp"
+    to_port     = 8080
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     protocol    = "-1"
